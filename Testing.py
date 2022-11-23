2584f5ca10323641c2ee654fad8440ee1873dc89
iryabts <iryabts@gmail.com>
2022-11-24 00:02:52 +0300

    understood what is "lexicographically" and how min works with strings

---
+++ Testing.py
@@ -0,0 +1,4 @@
+a = input("Введите строку а: ")
+b = input("Введите строку b: ")
+print(min(a))
+print(min(a), min(a, b))
--- Yandex_chapter_2.2_T.py
+++ Yandex_chapter_2.2_T.py
@@ -16,29 +16,27 @@ else:
 len_1 = len(str_1)
 len_2 = len(str_2)
 len_3 = len(str_3)
-if repl_1 and repl_2 and repl_3 and len_1 == min(len_1, len_2, len_3):
+if repl_1 and repl_2 and repl_3 and str_1 == min(str_1, str_2, str_3):
     print(str_1, len_1)
-elif repl_1 and repl_2 and repl_3 and len_2 == min(len_1, len_2, len_3):
+elif repl_1 and repl_2 and repl_3 and str_2 == min(str_1, str_2, str_3):
     print(str_2, len_2)
-elif repl_1 and repl_2 and repl_3 and len_3 == min(len_1, len_2, len_3):
+elif repl_1 and repl_2 and repl_3 and str_3 == min(str_1, str_2, str_3):
     print(str_3, len_3)
-elif not repl_1 and repl_2 and repl_3 and len_2 == min(len_2, len_3):
+elif not repl_1 and repl_2 and repl_3 and str_2 == min(str_2, str_3):
     print(str_2, len_2)
-elif not repl_1 and repl_2 and repl_3 and len_3 == min(len_2, len_3):
+elif not repl_1 and repl_2 and repl_3 and str_3 == min(str_2, str_3):
     print(str_3, len_3)
-elif repl_1 and not repl_2 and repl_3 and len_1 == min(len_1, len_3):
+elif repl_1 and not repl_2 and repl_3 and str_1 == min(str_1, str_3):
     print(str_1, len_1)
-elif repl_1 and not repl_2 and repl_3 and len_3 == min(len_1, len_3):
+elif repl_1 and not repl_2 and repl_3 and str_3 == min(str_1, str_3):
     print(str_3, len_3)
-elif repl_1 and repl_2 and not repl_3 and len_2 == min(len_1, len_2):
+elif repl_1 and repl_2 and not repl_3 and str_2 == min(str_1, str_2):
     print(str_2, len_2)
-elif repl_1 and repl_2 and not repl_3 and len_1 == min(len_1, len_2):
+elif repl_1 and repl_2 and not repl_3 and str_1 == min(str_1, str_2):
     print(str_1, len_1)
 elif repl_1 and not repl_2 and not repl_3:
     print(str_1, len_1)
 elif repl_2 and not repl_1 and not repl_3:
     print(str_2, len_2)
 elif repl_3 and not repl_1 and not repl_2:
-    print(str_3, len_3)
-elif repl_1 and repl_2 and repl_3 and len_1 == len_2 == len_3 == min(len_1, len_2, len_3):
     print(str_3, len_3)
