281485df2b0e1895a4202be17ac7261bb08494fa
iryabts <iryabts@gmail.com>
2022-11-24 14:13:22 +0300

    understood what is "lexicographically" and how min works with strings 24.11.2022

--- Yandex_chapter_2.2_I.py
+++ Yandex_chapter_2.2_I.py
@@ -1,11 +1,4 @@
 name_1 = input()
 name_2 = input()
 name_3 = input()
-if len(name_1) < len(name_2) and len(name_1) < len(name_3):
-    print(name_1)
-elif len(name_2) < len(name_1) and len(name_2) < len(name_3):
-    print(name_2)
-else:
-    print(name_3)
-# elif len(name_3) <= len(name_2) and len(name_3) <= len(name_1):
-#    print(name_3)
+print(min(name_1, name_2, name_3))
