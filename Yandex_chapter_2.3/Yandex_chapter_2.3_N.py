dace8496daa1e9cc68797466815150170f9b9ba1
iryabts <iryabts@gmail.com>
2022-12-25 14:43:47 +0300

    24.12.2022 cont. after inactive period

--- Yandex_chapter_2.3/Yandex_chapter_2.3_N.py
+++ Yandex_chapter_2.3/Yandex_chapter_2.3_N.py
@@ -4,9 +4,9 @@ c = 0
 for i in range(1, n + 1):
     if n % i == 0:
         m += 1
-    elif n % i != 0:
-        c += 1
-if m == 2 and c == n - 2:
+#    elif n % i != 0:
+#        c += 1
+if m == 2: # and c == n - 2:
     print("YES")
 else:
     print("NO")
