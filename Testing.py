7b4f55c9f1cf0592b6f00f71d5523baece2c397c
iryabts <iryabts@gmail.com>
2022-12-26 22:28:31 +0300

    26.12.2022 algorithm for simple numbers optimized by ** 0.5

--- Testing.py
+++ Testing.py
@@ -1,4 +1,10 @@
 n = int(input())
-print(n ** 0.5)
-
-
+m = 0
+p = int(n ** 0.5)
+for i in range(2, p + 1):
+    if n % i == 0:
+        m += 1
+if m == 0 and n != 1:
+    print("YES")
+else:
+    print("NO")
--- Yandex_chapter_2.3/Yandex_chapter_2.3_N.py
+++ Yandex_chapter_2.3/Yandex_chapter_2.3_N.py
@@ -1,12 +1,12 @@
 n = int(input())
 m = 0
-c = 0
-for i in range(1, int(n ** 0.5)):
+p = int(n ** 0.5)
+for i in range(1, p):
     if n % i == 0:
         m += 1
-    elif n % i != 0:
-        c += 1
-if m == 2 and c == n ** 0.5 - 2:
+        if m > 2:
+            break
+if m == 2:
     print("YES")
 else:
     print("NO")
